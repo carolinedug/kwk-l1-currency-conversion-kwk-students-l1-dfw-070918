3b88fdb84983d601737ccc65a3af7bf0ec799dd4
caroline dugan <caroline.dugan@gmail.com>
2018-07-13 14:40:36 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,15 @@
-# Write your cod here
+def usd_to_eur(dollar)
+  dollar/1.2
+end
+
+def eur_to_usd(euro)
+  euro*1.2
+end
+def usd_to_gbp(dollar)
+  dollar/0.76
+end
+
+def gpb_to_usd(pounds)
+  pouds*0.76
+end
+  
